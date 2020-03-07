4b10fa46f562e6e039a5221f9d1d8576d732a9a0
Shaoqi <50268786+ShaoqiChen@users.noreply.github.com>
2020-03-07 08:28:28 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	ver <- NULL
+	set <- function(y) {
+			x <<- y
+			ver <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) ver <<- inverse
+	getinverse <- function() ver
+	list(set = set, get = get,
+		 setinverse = setinverse,
+		 getinverse = getinverse)
 
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	ver <- x$getinverse()
+	if(!is.null(ver)) {
+			message("getting cached data")
+			return(ver)
+	}
+	matrix <- x$get()
+	ver <- solve(matrix, ...)
+	x$setinverse(ver)
+	ver
+		## Return a matrix that is the inverse of 'x'
 }
